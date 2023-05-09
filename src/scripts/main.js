dc66d789092909508a99d7998c06e1f9fb678865
Bryan Vera <brayanandresveragar@gmail.com>
2023-05-09 18:44:20 -0500

    :construction: JavaScript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,14 +7,35 @@ function capitalizeFirstLetter(str) {
 function humanizeDate($date, i) {
     const datetime = $date.getAttribute("datetime");
     const humanizedDate = humanize(datetime)
-    console.log(humanizedDate);
+    // console.log(humanizedDate);
 
     $date.textContent = capitalizeFirstLetter(humanizedDate)
     
 }
 
-const $dates = document.querySelectorAll("time")
-$dates.forEach(humanizeDate)
+function sortByDate($a, $b) {
+    const dateA = new Date($a.querySelector("time").getAttribute("datetime"));
+    const dateB = new Date($b.querySelector("time").getAttribute("datetime"));
+    return dateB - dateA;
+}
 
+function humanizeArticlesDate() {
+    const $dates = document.querySelectorAll("time")
+    $dates.forEach(humanizeDate);
+}
 
+function sordArticlesByDate() {
+    const $cards = document.querySelectorAll(".post-list .card");
+    const $cardsSorted = [...$cards].sort(sortByDate);
+    const $postList = document.querySelector(".post-list");
 
+    [...$postList.children].forEach($child => $child.remove());
+    $postList.append(...$cardsSorted)
+}
+
+function main() {
+    humanizeArticlesDate()
+    sordArticlesByDate()
+}
+
+main()
